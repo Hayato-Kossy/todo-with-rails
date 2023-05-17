d8389fb9b9ff00308ff08c5b98da1cdc5ce3794f
Hayato Koshi <kshyt2501@icloud.com>
2023-05-17 13:54:26 +0900

    done

---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Editor from './Editor';
+import './App.css';
+import { ToastContainer } from 'react-toastify';
+const App = () => (
+    <>
+    <Routes>
+        <Route path="events/*" element={<Editor />} />
+    </Routes>
+    <ToastContainer></ToastContainer>
+    </>
+);
+
+export default App;
